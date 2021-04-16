df59376d3c60973989451f8da0c648b5ec775813
johnmagcale <johnmichaelmagcale@gmail.com>
2021-04-16 16:41:20 +0800

    effect on menu

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
 
-      window.addEventListener(
-        "scroll",
-        () => {
-          document.body.style.setProperty(
-            "--scroll",
-            window.pageYOffset /
-              (document.body.offsetHeight - window.innerHeight)
-          );
-        },
-        false
-      );
+// $(window).scroll(function () {
+//   var scrollPos = $(this).scrollTop();
+//   $(".main-container").css({
+//     "background-size": 100 + scrollPos + "%",
+//   });
+// });
 
+// REWORK THE TOGGLEMENU ON MOBILE. You can add ::before to replace the spans to X to close it
 
+var menuList = document.getElementById("menuList");
+var closeButton = document.getElementById("close-button");
 
+menuList.style.maxHeight = "0px";
+closeButton.style.maxHeight = "0px";
 
-// REWORK THE TOGGLEMENU ON MOBILE. You can add ::before to replace the spans to X to close it 
-
-      var menuList = document.getElementById("menuList");
-      var closeButton = document.getElementById("close-button");
-
-      menuList.style.maxHeight = "0px";
-      closeButton.style.maxHeight = "0px";
-
-      function togglemenu() {
-        if (menuList.style.maxHeight == "0px" && closeButton.style.maxHeight =="0px") {
-          menuList.style.maxHeight = "100vh";
-        } else {
-          menuList.style.maxHeight = "0px";
-        }
-      }
-
+function togglemenu() {
+  if (
+    menuList.style.maxHeight == "0px" &&
+    closeButton.style.maxHeight == "0px"
+  ) {
+    menuList.style.maxHeight = "100vh";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
